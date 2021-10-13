f09df775efa8dcbc39e94afde1b5a4863a87e6fd
Jon Olivet Olivet <jolivet@transbank.cl>
2021-10-13 10:45:49 -0300

    Added localized strings.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "CommonsPack",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CommonsPack",
             targets: ["CommonsPack"]),
@@ -17,12 +16,17 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.2.0")),
         .package(url: "https://github.com/konkab/AlamofireNetworkActivityLogger.git", .upToNextMajor(from: "3.4.0")),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.1.0")),
-        .package(url: "https://github.com/jrendel/SwiftKeychainWrapper.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/jrendel/SwiftKeychainWrapper.git", .upToNextMajor(from: "4.0.0")),
+        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "3.2.1")
     ],
     targets: [
         .target(
             name: "CommonsPack",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .copy("Resources")
+            ]
+        ),
         .testTarget(
             name: "CommonsPackTests",
             dependencies: ["CommonsPack"]),
